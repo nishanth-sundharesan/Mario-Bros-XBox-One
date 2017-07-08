ac73796cdd38ca4797b2903f9720e9fb8f288a4a
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-07-08 17:51:31 -0400

    Added more functions for the BaseGameState. All the GameStates inherit from this class.

--- Source/Game.Universal/BaseGameState.cpp
+++ Source/Game.Universal/BaseGameState.cpp
@@ -3,8 +3,29 @@
 
 namespace GameStates
 {
-	BaseGameState::BaseGameState(const std::shared_ptr<DX::DeviceResources>& deviceResources)
-		:DrawableGameComponent(deviceResources)
+	BaseGameState::BaseGameState(const std::shared_ptr<DX::DeviceResources>& deviceResources, const std::shared_ptr<DX::Camera>& camera,
+		const std::shared_ptr<DX::KeyboardComponent>& keyboard, const std::shared_ptr<DX::GamePadComponent>& gamePad)
+		:mDeviceResources(deviceResources), mCamera(camera), mKeyboard(keyboard), mGamePad(gamePad)
+	{
+	}
+
+	void BaseGameState::StateEntering()
+	{
+	}
+
+	void BaseGameState::StateLeaving()
+	{
+	}
+
+	void BaseGameState::CreateDeviceDependentResources()
+	{
+	}
+
+	void BaseGameState::CreateWindowSizeDependentResources()
+	{
+	}
+
+	void BaseGameState::ReleaseDeviceDependentResources()
 	{
 	}
 }
--- Source/Game.Universal/BaseGameState.h
+++ Source/Game.Universal/BaseGameState.h
@@ -1,20 +1,44 @@
 #pragma once
-#include "DrawableGameComponent.h"
 #include <DirectXMath.h>
 #include <vector>
 
+namespace DX
+{
+	class DeviceResources;
+	class Camera;
+}
+
 namespace GameStates
 {
-	class BaseGameState : public DX::DrawableGameComponent
+	enum class GameState
+	{
+		GAME_START,
+		GAME_PLAY,
+		GAME_OVER
+	};
+
+	class BaseGameState
 	{
 	public:
-		BaseGameState(const std::shared_ptr<DX::DeviceResources>& deviceResources);
-		BaseGameState(const BaseGameState&) = default;
-		BaseGameState& operator=(const BaseGameState&) = default;
-		BaseGameState(BaseGameState&&) = default;
-		BaseGameState& operator=(BaseGameState&&) = default;
+		BaseGameState(const std::shared_ptr<DX::DeviceResources>& deviceResources, const std::shared_ptr<DX::Camera>& camera, const std::shared_ptr<DX::KeyboardComponent>& keyboard, const std::shared_ptr<DX::GamePadComponent>& gamePad);
+		BaseGameState(const BaseGameState&) = delete;
+		BaseGameState& operator=(const BaseGameState&) = delete;
+		BaseGameState(BaseGameState&&) = delete;
+		BaseGameState& operator=(BaseGameState&&) = delete;
 		virtual ~BaseGameState() = default;
-		virtual void StateEntering() = 0;
-		virtual void StateLeaving() = 0;
+		virtual void StateEntering();
+		virtual void Update(const DX::StepTimer& timer) = 0;
+		virtual void Render(const DX::StepTimer& timer) = 0;
+		virtual void StateLeaving();
+
+		virtual void CreateDeviceDependentResources();
+		virtual void CreateWindowSizeDependentResources();
+		virtual void ReleaseDeviceDependentResources();
+
+	protected:
+		std::shared_ptr<DX::DeviceResources> mDeviceResources;
+		std::shared_ptr<DX::Camera> mCamera;
+		std::shared_ptr<DX::KeyboardComponent> mKeyboard;
+		std::shared_ptr<DX::GamePadComponent> mGamePad;
 	};
 }
