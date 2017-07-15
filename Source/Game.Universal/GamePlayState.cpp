5d44202b8c87dc5441fd7d5419b943d1ef3133dc
Nishanth Sundharesan <nishanth.reddy992@gmail.com>
2017-07-14 20:27:47 -0400

    Implemented functionality for the GamePlayState.

--- Source/Game.Universal/GamePlayState.cpp
+++ Source/Game.Universal/GamePlayState.cpp
@@ -1,34 +1,95 @@
 #include "pch.h"
 #include "GamePlayState.h"
+#include "Background.h"
+#include "Player.h"
+#include "Blocks.h"
+#include "PhysicsManager.h"
+#include "EnemyManager.h"
+#include "LevelManager.h"
 
 using namespace DX;
+using namespace std;
+using namespace GameEntity;
+using namespace Managers;
 
 namespace GameStates
 {
-	GamePlayState::GamePlayState(const std::shared_ptr<DX::DeviceResources>& deviceResources, const std::shared_ptr<DX::Camera>& camera)
-		:BaseGameState(deviceResources)
-	{
-		//TODO 
-		camera;
+	GamePlayState::GamePlayState(const shared_ptr<DeviceResources>& deviceResources, const shared_ptr<Camera>& camera, const shared_ptr<KeyboardComponent>& keyboard, const shared_ptr<GamePadComponent>& gamePad)
+		:BaseGameState(deviceResources, camera, keyboard, gamePad)
+	{		 
+		auto background = make_shared<Background>(mDeviceResources, mCamera);
+
+		auto blocks = make_shared<Blocks>(mDeviceResources, mCamera);
+
+		mPlayer1 = make_shared<Player>(mDeviceResources, mCamera, mKeyboard, mGamePad);
+		
+		mLevelManager = make_shared<LevelManager>(mDeviceResources, mCamera, mPlayer1);
+
+		mEnemyManager = make_shared<EnemyManager>(mDeviceResources, mCamera, mLevelManager);
+
+		auto physicsManager = make_shared<PhysicsManager>(mDeviceResources, mEnemyManager, mPlayer1, blocks);
+
+		blocks->Init(*physicsManager);
+		mLevelManager->SetEnemyManager(mEnemyManager);
+
+		mComponents.push_back(blocks);
+		mComponents.push_back(mEnemyManager);
+		mComponents.push_back(background);		
+		mComponents.push_back(mPlayer1);
+		mComponents.push_back(mLevelManager);
+		mComponents.push_back(physicsManager);					
 	}
 
 	void GamePlayState::StateEntering()
 	{
+		//Add blocks,Init as well
+		mPlayer1->ResetScoreAndLives();
+		mLevelManager->ResetLevelManager();
+		mLevelManager->LoadNextLevel();
+		mEnemyManager->ResetEnemyManager();		
 	}
 
 	void GamePlayState::Update(const StepTimer& timer)
 	{
-		//TODO
-		timer;
+		for (auto& component : mComponents)
+		{
+			component->Update(timer);
+		}		
 	}
 
 	void GamePlayState::Render(const StepTimer& timer)
 	{
-		//TODO
-		timer;
+		for (auto& component : mComponents)
+		{
+			auto drawableComponent = dynamic_pointer_cast<DrawableGameComponent>(component);
+			if (drawableComponent != nullptr && drawableComponent->Visible())
+			{
+				drawableComponent->Render(timer);
+			}
+		}
 	}
 
-	void GamePlayState::StateLeaving()
+	void GamePlayState::CreateDeviceDependentResources()
 	{
+		for (auto& component : mComponents)
+		{
+			component->CreateDeviceDependentResources();
+		}
+	}
+
+	void GamePlayState::CreateWindowSizeDependentResources()
+	{
+		for (auto& component : mComponents)
+		{
+			component->CreateWindowSizeDependentResources();
+		}
+	}
+
+	void GamePlayState::ReleaseDeviceDependentResources()
+	{
+		for (auto& component : mComponents)
+		{
+			component->ReleaseDeviceDependentResources();
+		}
 	}
 }
--- Source/Game.Universal/GamePlayState.h
+++ Source/Game.Universal/GamePlayState.h
@@ -1,21 +1,42 @@
 #pragma once
 #include "BaseGameState.h"
 
+namespace GameEntity
+{
+	class Player;
+}
+
+namespace Managers
+{
+	class LevelManager;
+	class EnemyManager;
+}
+
 namespace GameStates
 {
 	class GamePlayState final : public BaseGameState
 	{
-		GamePlayState(const std::shared_ptr<DX::DeviceResources>& deviceResources, const std::shared_ptr<DX::Camera>& camera);
-		GamePlayState(const GamePlayState&) = default;
-		GamePlayState& operator=(const GamePlayState&) = default;
-		GamePlayState(GamePlayState&&) = default;
-		GamePlayState& operator=(GamePlayState&&) = default;
+	public:
+		GamePlayState(const std::shared_ptr<DX::DeviceResources>& deviceResources, const std::shared_ptr<DX::Camera>& camera, const std::shared_ptr<DX::KeyboardComponent>& keyboard, const std::shared_ptr<DX::GamePadComponent>& gamePad);
+		GamePlayState(const GamePlayState&) = delete;
+		GamePlayState& operator=(const GamePlayState&) = delete;
+		GamePlayState(GamePlayState&&) = delete;
+		GamePlayState& operator=(GamePlayState&&) = delete;
+		~GamePlayState() = default;
 
 		virtual void StateEntering() override;
 		virtual void Update(const DX::StepTimer& timer) override;
-		virtual void Render(const DX::StepTimer& timer) override;
-		virtual void StateLeaving() override;
+		virtual void Render(const DX::StepTimer& timer) override;		
 
-		~GamePlayState() = default;
+		virtual void CreateDeviceDependentResources() override;
+		virtual void CreateWindowSizeDependentResources() override;
+		virtual void ReleaseDeviceDependentResources() override;
+
+	private:
+		std::shared_ptr<GameEntity::Player> mPlayer1;
+		std::shared_ptr<Managers::LevelManager> mLevelManager;
+		std::shared_ptr<Managers::EnemyManager> mEnemyManager;
+
+		std::vector<std::shared_ptr<DX::GameComponent>> mComponents;
 	};
 }
